8bfaf3ab0170cf9f53938a9dea958f3b051bbcd1
jeeva-git <jeevanandham_18@yahoo.co.in>
2020-01-24 21:02:18 +0800

    Add files via upload
    added spring basic files

---
+++ src/main/java/com/example/restserviceSample/RestserviceSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.restserviceSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestserviceSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestserviceSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restserviceSample/RestserviceSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restserviceSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestserviceSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

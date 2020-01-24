05c85a4b7ee1c811914246fa05b849f88a42dd7b
jeeva <jeeva@localhost>
2020-01-24 21:34:20 +0800

    test commit

--- src/main/java/com/example/restserviceSample/RestserviceSampleApplication.java
+++ src/main/java/com/example/restserviceSample/RestserviceSampleApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestserviceSampleApplication {
 
+	//test commit
 	public static void main(String[] args) {
 		SpringApplication.run(RestserviceSampleApplication.class, args);
 	}
